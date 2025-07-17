c8d66c74dc3706ca6c9012023605f6a689673cb3
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-18 01:00:22 +0600

    Add array-methods.js with split and join examples

---
+++ array-methods.js
@@ -0,0 +1,9 @@
+
+/* muloto ei string guloke array te convert kora (split(" ")) and also use of join -- syntex --.join("/")
+* word gular majhe space ache muloto ei space gulake dhore kete kete slice kore array te convert korbo
+*/
+const x = "My name is programming hero"
+const words = x.split(" ");
+console.log(words);
+const y = words.join("/")
+console.log(y);
