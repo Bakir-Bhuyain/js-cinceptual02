3d21f47e9d184d8062d8cb781c06d110e6065e69
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-18 02:00:44 +0600

    Add for...in loop to print myPet properties

---
+++ object.js
@@ -0,0 +1,78 @@
+//OBJECT notation about :
+// const objectName = {
+// propertyName: propertyValue;
+// }
+
+// const name = "Travis"
+// const age = 2;
+// const isMale = true;
+// const food = 'fish';
+// const color = 'white';
+// const specis = 'Hulu Biral';
+
+// const myPet ={
+//       name: 'travis',
+//       age: 2,
+//       color: 'white',
+//       isMale: true,
+//       species :'hulu biral',
+//       food: 'fish'
+// };
+
+// const catFood = myPet.food;
+// console.log(catFood);
+// const catFood = myPet["food"];
+// console.log(catFood);
+//bracket notation
+//jodi propertyname e space thake ba hipen thake ba kono number thake tkhn amra dot diye access korte parbo na bracket diye access korte hbe;
+//console.log = myPet["food"];
+//dot notation
+// console.log(myPet.name);
+
+/* object er vhitor kono ekta property value k change kore hole */
+// myPet.color = 'black';
+// console.log(myPet);
+
+/* use of bracket notation for changing of property values */
+// myPet["isMale"] = false;
+
+/* new property add kora */
+// myPet.sound = 'meo meo';
+
+// delete myPet.age;
+// console.log(myPet);
+
+
+// const myPet ={
+//       name: 'travis',
+//       age: 2,
+//       color: 'white',
+//       isMale: true,
+//       species :'hulu biral',
+//       food: 'fish'
+// };
+
+/**
+ * object e loop chalaite hole for of kaj kore na 
+ * for in loop kaj kore 
+*/
+
+// for (let key in myPet){
+//       console.log(key);
+      // console.log(myPet[key]);
+      // console.log(key);
+// }
+
+
+// const student = {
+//       name :{
+//             firstName : 'Bakir',
+//             middleName : 'Bhuyain',
+//             lastName : 'Hemal',
+//       },
+//       isMale : true,
+//       subjects : ['bangla', 'english'],
+//       roll : 34,
+// };
+// console.log(student.name.middleName);
+// console.log(student.subjects[1]);
