aff40f8ceecd39ea8f5d6321e510003ebfa9544b
Bakir Bhuyain <hemalbakir@gmail.com>
2025-07-18 00:51:37 +0600

    Update string.js

---
+++ string.js
@@ -0,0 +1,37 @@
+//string compare lowercase uppercase trim
+/**
+ * toLowerCase()
+ * toUpperCase()
+ */
+// const x = 'hello';
+// const y = 'hEllo';
+
+// if ( x.toLocaleLowerCase() === y.toLocaleLowerCase()){
+//       console.log('both are same');
+
+// }else{
+//       console.log('both are different');
+// }
+
+/**
+ * includes
+*/
+
+// const x ="Hello world";
+// const y = "hello";
+
+// // console.log(x.includes(y));
+
+// console.log(x.toLowerCase().includes(y.toLowerCase()));
+
+/* trim */
+/* trim shudhu suru and sesh er unwanted space thakle soray dey but majh khaner space thakle kaj korbe na */
+// const x = 'hello';
+// const y = 'hello    ';
+
+// if(x.trim().toLowerCase() === y.trim().toLowerCase()){
+//       console.log("both are same");
+// }else{
+
+//       console.log("both are different");
+// }
